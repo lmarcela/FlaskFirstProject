3314b281dcbafd617bf74389ddbbd52430351abe
Marcela Malaver <marcela9409@gmail.com>
2018-03-05 14:29:44 -0500

    Generar html de forma dinamica enviando variables

--- run_app.py
+++ run_app.py
@@ -1,8 +1,8 @@
 import os
 
 # from library._01_simple import app
-from library._02_html_inside_view import app
-# from library._03_template_str_inside_view import app
+# from library._02_html_inside_view import app
+from library._03_template_str_inside_view import app
 # from library._04_template_outside_view import app
 # from library._05_basic_routing import app
 # from library._06_raising_custom_errors import app
