fd0571d5d34cd11003d9a8e55a11663d1b8c5864
Marcela Malaver <marcela9409@gmail.com>
2018-03-05 14:11:20 -0500

    Proyecto base descargado

---
+++ README.md
@@ -0,0 +1,31 @@
+# FlaskFirstProject
+
+
+
+## PASOS
+
+PASO 1) Seguir los pasos (1-10) para creacion de ambiente (test19) y activacion del mismo disponibles en README.md de https://github.com/lmarcela/DjangoFirstApp
+
+PASO 2) Activar el ambiente, instalar Flask, ejecutar archivo :
+
+		E:\PYTHON\DJANGO\ambientes\test19\Scripts>activate
+		(test19) E:\PYTHON\DJANGO\ambientes\test19\Scripts>pip install Flask
+
+		(test19) E:\PYTHON\DJANGO\ambientes\test19>cd E:\PYTHON\FLASK\Tutorial Udemy\flask_introduction
+
+		(test19) E:\PYTHON\FLASK\Tutorial Udemy\flask_introduction>python run_app.py
+
+		NOTA: Se descargo proyecto de base de https://github.com/rmotr/flask-introduction
+
+
+## Comandos Flask:
+
+- Empezar app: python run_app.py
+
+## GIT COMMANDS:
+		git init
+		git add .
+		git commit -m "first commit"
+		git git remote add origin https://github.com/lmarcela/FlaskFirstProject.git
+		git push -u origin master
+
---
+++ library/_02_html_inside_view.py
@@ -0,0 +1,35 @@
+"""In this example, we're returning valid HTML code.
+
+If you open this example in your browser, you'll see the page rendered
+nicely as a real HTML page. That means that Flask took care of returning
+to the client the `Content-Type: 'text/html'`.
+
+**TODO**
+Use the list of authors present in the `hello_world` view to
+return a <ul> HTML tag containing the authors in the list.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    html = """
+        <html>
+            <h1>Welcome to our Library!</h1>
+            <ul>
+                <li>Alan Poe</li>
+                <li>Jorge L. Borges</li>
+                <li>Mark Twain</li>
+            </ul>
+        </html>
+    """
+    authors = ["Alan Poe", "Jorge L. Borges", "Mark Twain"]
+    authors_list = "<ul>"
+    authors_list += "\n".join([
+        "<li>{author}</li>".format(author=author) for author in authors])
+    authors_list+="</ul>"
+    print(authors_list)
+    # build an <ul> with authors
+    return html
